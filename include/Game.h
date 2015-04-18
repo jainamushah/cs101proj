7b5f5f5d261b2b4751394f9da22cc436bc1dd02e
jainamushah <jainamushah20101996@gmail.com>
2015-04-18 16:42:16 +0530

    comments added
    commented game.h

--- include/Game.h
+++ include/Game.h
@@ -11,44 +11,44 @@ public:
 Game();
 void run();
 sf::RenderWindow mWindow;
-double vx_puck=0,vy_puck=1.1*sqrt(2);
+double vx_puck=0,vy_puck=1.1*sqrt(2);    //DEFINES ALL VELOCITIES OF PUCK,AI AND PLAYER
 double vx_ai=0.4,vy_ai=0.4;
 double vx_player,vy_player;
 double vx_path,vy_path;
-int s1=0,s2=0;
-int algo_select=1;
+int s1=0,s2=0;                  //FOR DISPLAY OF SCORE
+int algo_select=1;              //FOR RANDOMLY SELECTING ALGORTIHM OF AI
 
 
 private:
 
-void processEvents(sf::Time dt);
+void processEvents(sf::Time dt);            //MAIN GAME LOOP
 
-void update();
+void update();                          //UPDATES POSITIONS OF PLAYERS
 
-void render();
+void render();                          //DRAWS EVERYTHING ON SCREEN
 
 sf::Vector2f handlePlayerInput(float x,float y);
 
-sf::Texture mTexture;
+sf::Texture mTexture;           // FOR PLAYER
 sf::Sprite mPlayer;
 
-sf::Vector2f movement;
+sf::Vector2f movement;                  //FOR PLAYER MOVEMENTT
 
 //sf::RectangleShape  arena;
 
-sf::Texture mArena;
+sf::Texture mArena;                 //FOR BOARD
 sf::Sprite iArena;
 
 sf::Texture mPuck;
-sf::Sprite sPuck;
+sf::Sprite sPuck;               //FOR PUCK
 
-sf::Texture mAi;
+sf::Texture mAi;                //FOR COMPUTER PLAYER
 sf::Sprite sAi;
 
-sf::Font font1;
+sf::Font font1;                 //FOR FONTS
 
 sf::Texture side_block;
-sf::Sprite s_side_block;
+sf::Sprite s_side_block;           // FOR SCORE SIDE TEXTURE
 
 sf::Texture win_screen;
 sf::Sprite s_win_screen;
@@ -56,7 +56,7 @@ sf::Sprite s_win_screen;
 sf::Texture lose_screen;
 sf::Sprite s_lose_screen;
 
-sf::Text text;
+sf::Text text;          //TEXTS FOR SCORE
 sf::Text text_;
 sf::Text text1_0;
 sf::Text text1_1;
